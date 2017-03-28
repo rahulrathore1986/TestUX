432e1c45c46a347034238e83455e8341037fbac8
rahkuma <rahkuma@RAHKUMA-WX-1>
2017-03-28 20:59:14 +0530

    Added comment to student class

--- ConsoleApplication1/Student.cs
+++ ConsoleApplication1/Student.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 
 namespace ConsoleApplication1
 {
+    /// <summary>
+    /// Test clas added for demo student
+    /// </summary>
     public class Student
     {
     }
