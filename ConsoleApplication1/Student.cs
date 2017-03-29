555d17b5e1dd5ca82526672f222516ee41cb8cb9
rahkuma <rahkuma@RAHKUMA-WX-1>
2017-03-29 12:31:17 +0530

    removed sth

--- ConsoleApplication1/Student.cs
+++ ConsoleApplication1/Student.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 namespace ConsoleApplication1
 {
     /// <summary>
-    /// Test clas added for demo student dgdfg mmm
+    /// Test clas added for demo student
     /// </summary>
     public class Student
     {
