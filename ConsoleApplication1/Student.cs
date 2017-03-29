ada35c89804f9f06e8ccdcad151b25a6ef6c9b44
rahkuma <rahkuma@RAHKUMA-WX-1>
2017-03-29 12:38:10 +0530

    a

--- ConsoleApplication1/Student.cs
+++ ConsoleApplication1/Student.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 namespace ConsoleApplication1
 {
     /// <summary>
-    /// Test clas added for demo student TEST Merge
+    /// Test clas added for demo student TEST291 Merge
     /// </summary>
     public class Student
     {
